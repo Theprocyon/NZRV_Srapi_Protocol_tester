9dd20528f3c59a9bd1b2f7b8d05214acf552b0bf
theprocyon <hyeon@vorasky.com>
2023-04-19 10:32:06 +0900

    - Initial commit

---
+++ src/Parser.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Parser
+    {
+        public const short STX = 0x02;
+        public const short GS = 0x1D;
+        public const short US = 0x1F;
+        public const short ETX = 0x03;
+
+        private const int PARSER_STRING_LENGTH_MAX = 120;
+        private enum State
+        {
+            PARSE_STATE_IDLE,
+            PARSE_STATE_GOT_STX,
+            PARSE_STATE_GOT_API,
+            PARSE_STATE_GOT_CHKSUM
+        }
+
+        State mParserState = State.PARSE_STATE_IDLE;
+
+        int mCount;
+        byte mChecksum;
+
+        StringBuilder mStringBuilder;
+        String mApiName;
+
+        public Parser() { Initialize(); }
+        public void Initialize()
+        {
+            mApiName = String.Empty;
+
+            if (mStringBuilder == null)
+                mStringBuilder = new StringBuilder();
+            mStringBuilder.Clear();
+
+            mCount = 0;
+            mParserState = State.PARSE_STATE_IDLE;
+            mChecksum = 0;
+        }
+        public String parseChar(byte _c)
+        {
+            switch (this.mParserState)
+            {
+                case State.PARSE_STATE_IDLE:
+                    if (_c == STX)
+                    {
+                        mParserState = State.PARSE_STATE_GOT_STX;
+                    }
+                    mCount = 0;
+                    break;
+
+                case State.PARSE_STATE_GOT_STX:
+                    mChecksum ^= _c;
+                    mCount++;
+                    if (_c == GS)
+                    {
+                        mApiName = mStringBuilder.ToString();
+                        mStringBuilder.Clear();
+                        mParserState = State.PARSE_STATE_GOT_API;
+                        mCount = 0;
+                        
+                        break;
+                    }
+                    if (mCount > PARSER_STRING_LENGTH_MAX)
+                    {
+                        parseFail("API length exceeds " + PARSER_STRING_LENGTH_MAX);
+                        break;
+                    }
+                    mStringBuilder.Append((char)_c);
+                    break;
+
+                case State.PARSE_STATE_GOT_API:
+                    mChecksum |= 0x20;
+                    Console.WriteLine("Checksum is " + (int)_c);
+                    if (mChecksum != _c)
+                    {
+                        parseFail("App Checksum : " + mChecksum + ", rcvd Checksum : " + _c);
+                        break;
+                    }
+                    this.mParserState = State.PARSE_STATE_GOT_CHKSUM;
+                    break;
+
+                case State.PARSE_STATE_GOT_CHKSUM:
+                    if (_c == ETX)
+                    {
+                        if (String.Equals(this.mApiName, "/api/module/v1/pmc/bat/status"))
+                        {
+                            return mApiName;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Parse result different api!!");
+                            return "";
+                        }
+                    }
+                    break;
+            }
+            return "";
+        }
+        private void parseFail(String _msg)
+        {
+            Console.WriteLine("Parse failed at State " + this.mParserState.ToString() + ", following reason. " + _msg);
+            Initialize();
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Text;
+using System.Diagnostics;
+using System.Collections.Concurrent;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static bool _continue;
+        static ConcurrentQueue<Object> _queue;
+        static ConcurrentQueue<byte> _readqueue;
+        static SerialPort _serialPort;
+        static Stopwatch _stopwatch;
+        static Parser _parser;
+        public static void Main()
+        {
+            StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+            Thread readThread = new Thread(Read);
+            Thread writerThread = new Thread(Write);
+            Thread parserThread = new Thread(parseByte);
+
+            _stopwatch = new Stopwatch();
+            _queue = new ConcurrentQueue<Object>();
+            _readqueue = new ConcurrentQueue<byte>();
+
+            _serialPort = new SerialPort();
+            _parser = new Parser();
+            _parser.Initialize();
+
+            _serialPort.PortName = SetPortName(_serialPort.PortName);
+            _serialPort.BaudRate = 115200;
+            _serialPort.Parity = Parity.None;
+            _serialPort.DataBits = 8;
+            _serialPort.StopBits = StopBits.One;
+            _serialPort.Handshake = Handshake.None;
+
+            Console.Clear();
+
+            _serialPort.ReadTimeout = 500;
+            _serialPort.WriteTimeout = 500;
+
+            try
+            {
+                _serialPort.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+
+            _continue = true;
+
+            readThread.Start();
+            writerThread.Start();
+            parserThread.Start();
+
+            readThread.Join();
+            writerThread.Join();
+            parserThread.Join();
+
+            _serialPort.Close();
+        }
+
+        public static void parseByte()
+        {
+            while (_continue)
+            {
+                if (!_readqueue.IsEmpty)
+                {
+                    byte _c;
+                    
+                    while (_readqueue.TryDequeue(out _c) == false) { Thread.Sleep(10); }
+
+                    if (_parser.parseChar(_c) != "")
+                    {
+                        if (_stopwatch.IsRunning)
+                        {
+                            _stopwatch.Stop();
+                            Console.WriteLine("Interval Time is : " + _stopwatch.ElapsedMilliseconds);
+                            _stopwatch.Restart();
+                        }
+                        else
+                        {
+                            _stopwatch.Start();
+                        }
+                        _parser.Initialize();
+                        _queue.Enqueue(new object());
+                    }
+                }
+            }
+        }
+
+        public static void Read()
+        {
+            while (_continue)
+            {
+                try
+                {
+                    int num = _serialPort.BytesToRead;
+                    byte[] rb = new byte[num];
+                    _serialPort.Read(rb, 0, num);
+
+                    for (int i = 0; i < num; i++)
+                    {
+                        _readqueue.Enqueue(rb[i]);
+                    }
+                }
+                catch (TimeoutException) { }
+            }
+        }
+        public static void Write()
+        {
+            while (_continue)
+            {
+                try
+                {
+                    if (!_queue.IsEmpty)
+                    {
+                        Object obj;
+                        while (_queue.TryDequeue(out obj) == false) { };
+
+                        Console.SetCursorPosition(Console.CursorTop, Console.CursorLeft);
+
+                        MemoryStream stream = new MemoryStream();
+                        BinaryWriter writer = new BinaryWriter(stream);
+                        writer.Write((byte)0x02);
+                        writer.Write(Encoding.ASCII.GetBytes("/api/module/v1/pmc/bat/status"));
+                        writer.Write((byte)0x1D);
+                        writer.Write((byte)0x00);
+                        writer.Write((byte)0xC8);
+                        writer.Write((byte)0x1F);
+                        writer.Write(Encoding.ASCII.GetBytes("{\r\n\"type\" : \"pmc\"\r\n}\r\n"));
+                        writer.Write((byte)0x1F);
+                        writer.Write((byte)0xB0); // Chksum
+                        writer.Write((byte)0x03);
+                        byte[] bf = stream.ToArray();
+
+                        _serialPort.Write(bf, 0, bf.Length);
+
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    //Swallow
+                }
+            }
+        }
+
+        public static string SetPortName(string defaultPortName)
+        {
+            string portName;
+
+            Console.WriteLine("Available Ports:");
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                Console.WriteLine("   {0}", s);
+            }
+
+            Console.Write("COM port({0}): ", defaultPortName);
+            portName = Console.ReadLine();
+
+            if (portName == "")
+            {
+                portName = defaultPortName;
+            }
+            return portName;
+        }
+    }
+}
